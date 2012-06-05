4d652b624df66e95dd9276250a328b40ee600ab4
Danielle Mei <danielle.mei1989@gmail.com>
2012-06-04 22:05:24 -0400

    string_simple new:replace_space, is_rotation

--- B/python/string_simple.py
+++ B/python/string_simple.py
@@ -79,9 +79,58 @@ def is_anagrams(s1, s2):
         return True
     return sorted(s1) == sorted(s2)
 
+# replace all spaces with '%20' - useful in url parse
+# python build-in function: string.replace(old, new, [count])
+# Note: cannot done in-place since pythong str is immutable
+def replace_space(s):
+    result = list()
+    i = 0
+    for c in s:
+        if c == ' ':
+            result.append('%')
+            result.append('2')
+            result.append('0')
+            i += 3
+        else:
+            result.append(c)
+            i += 1
+    return ''.join(result)
+
+# follow-up: replace(old, new)...
+# Note: when concat string in python, list's join() is usually fastest
+def my_replace(s, old, new):
+    indices = set([i.start() for i in re.finditer(old, s)]) # the complexity...
+    if len(indices) == 0:
+        print 'no old in the string'
+        return s # no occurence
+    
+    result = list()
+    i = 0 
+    while i < len(s):
+        if (i in indices):
+            for c in new:
+                result.append(c)
+                i += 1
+        else:
+            result.append(s[i])
+            i += 1
+
+    return ''.join(result)
+
 
 # remove duplicate chars in a string without extra space
 #def remove_dup(s):
+
+
+# check if s2 is a rotation of s1 using is_substring
+def is_rotation(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    def is_substring(s1, s2):
+        return s1.find(s2) != -1
+
+    return is_substring(s1 + s1, s2)
 
 
 # Simple provided test() function used in main() to print
@@ -110,14 +159,19 @@ def main():
     # string...
     cases = [
             'abcdwsef',
-            'hhh',
-            'I love rongchao!!!@_@'
+            'h hh',
+            'I love rongchao, hope rongchao love me too @_@'
             ]
     for c in cases:
         test(reverse_str(c), c[::-1])
         test(is_chars_unique(c, False), False)
+        test(replace_space(c), c.replace(' ', '%20'))
+        test(my_replace(c, 'rongchao', 'Danielle'), 
+                c.replace('rongchao', 'Danielle'))
 
     test(is_anagrams('abeed23', 'e2aeb3d'), True)
+    test(is_rotation('waterbottle', 'erbottlewat'), True)
+    test(is_rotation('ri', 'i'), False)
 
 if __name__ == '__main__':
     main()
